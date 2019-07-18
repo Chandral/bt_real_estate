838fe174dd6cf3de5de1f6111d8900905980157b
unknown <chandral.thakor@gmail.com>
2019-07-18 10:56:17 +0530

    Added database configuration and migrated default migrations.

--- btre/settings.py
+++ btre/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': CONFIG_DATA["NAME"],
+        'USER': CONFIG_DATA["USER"],
+        'PASSWORD': CONFIG_DATA["PASSWORD"],
+        'HOST': CONFIG_DATA["HOST"]
     }
 }
 
