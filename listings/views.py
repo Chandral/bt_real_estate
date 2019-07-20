bb8b35864640818a960093340548e5eeb5998462
unknown <chandral.thakor@gmail.com>
2019-07-20 12:15:44 +0530

    Listings pages now displays data from the database.

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Listing
 
 
 def index(request):
-    return render(request, "listings/listings.html")
+    listings = Listing.objects.all()
+    context = {"listings": listings}
+    return render(request, "listings/listings.html", context)
 
 
 def listing(request):
