537534d2f14c59aad6e25cb47622c1c6eb7cbe4d
unknown <chandral.thakor@gmail.com>
2019-07-22 16:03:18 +0530

    Can now register users.

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error messages')
-        return redirect('register')
-    return render(request, "accounts/register.html")
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username taken")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email,
+                                                first_name=first_name, last_name=last_name)
+                user.save()
+                messages.success(request, "Registered")
+                return redirect('login')
+        else:
+            messages.error(request, "Passwords don't match")
+            return redirect('register')
+    return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, "accounts/login.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user != None:
+            auth.login(request, user)
+            messages.success(request, "Logged in")
+            return redirect('dashboard')
+        else:
+            messages.success(request, "Invalid credentials")
+        return redirect('login')
+    return render(request, 'accounts/login.html')
 
 
 def logout():
