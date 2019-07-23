4abf42f9aa948ec9cc764c8510d75ece36a71bcb
unknown <chandral.thakor@gmail.com>
2019-07-23 15:37:07 +0530

    User's dashboard now shows list of inquiries made.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
+from contacts.models import Contact
 
 
 def register(request):
@@ -50,4 +51,6 @@ def logout(request):
 
 
 def dashboard(request):
-    return render(request, "accounts/dashboard.html")
+    contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {'contacts': contacts}
+    return render(request, "accounts/dashboard.html", context)
